cc817e8b544e48efdda1ef5a6aedfd943f4f44f2
Andre Mata <=>
2020-09-06 09:41:35 -0300

    Tratamento de erro

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -2,7 +2,7 @@ import { Product } from './product.model';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, EMPTY } from 'rxjs';
 import { throwMatDuplicatedDrawerError } from '@angular/material/sidenav';
 import { catchError, map } from 'rxjs/operators';
 
@@ -20,30 +20,51 @@ export class ProductService {
     this.snackBar.open(msg, 'X',{
       duration: 3000,
       horizontalPosition: "right",
-      verticalPosition: "top"
+      verticalPosition: "top",
+      panelClass: isError ? ['msg-error'] : ['msg-success']
     })
   }
 
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.urlEndPoint, product)
+    return this.http.post<Product>(this.urlEndPoint, product).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
 
   read(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.urlEndPoint)
+    return this.http.get<Product[]>(this.urlEndPoint).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
 
   readById(id: number): Observable<Product> {
     const url = `${this.urlEndPoint}/${id}`
-    return this.http.get<Product>(url)
+    return this.http.get<Product>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
 
   update(product: Product): Observable<Product> {
     const url = `${this.urlEndPoint}/${product.id}`
-    return this.http.put<Product>(url, product)
+    return this.http.put<Product>(url, product).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
   }
 
   delete(id: number): Observable<Product> {
     const url = `${this.urlEndPoint}/${id}`
-    return this.http.delete<Product>(url)
+    return this.http.delete<Product>(url).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    )
+  }
+  
+  errorHandler(e: any): Observable<any> {
+    this.showMessage("Ocorreu um erro!", true)
+    return EMPTY
   }
 }
